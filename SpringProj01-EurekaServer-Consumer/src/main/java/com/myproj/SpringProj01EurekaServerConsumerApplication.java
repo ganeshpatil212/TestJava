5abf7ee09d095247a1be12c8c45d92c64a9ec004
Admin <Admin@host.docker.internal>
2026-01-29 01:36:09 +0530

    initial git commit

---
+++ SpringProj01-EurekaServer-Consumer/src/main/java/com/myproj/SpringProj01EurekaServerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.myproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProj01EurekaServerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProj01EurekaServerConsumerApplication.class, args);
+	}
+
+}
