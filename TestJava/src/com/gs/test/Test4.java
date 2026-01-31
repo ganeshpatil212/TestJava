3d8045055f2bbbf3a09d4b4ff35f17dea0a5c75b
Admin <Admin@host.docker.internal>
2026-01-31 22:52:38 +0530

    test example

---
+++ TestJava/src/com/gs/test/Test4.java
@@ -0,0 +1,34 @@
+package com.gs.test;
+
+class A{
+	int i = 10;
+	void display() {
+		System.out.println("Class A: i = " + i);
+	}
+}
+
+class B extends A{
+	int j = 20;
+	void display() {
+		System.out.println("Class B: j = " + j);
+	}
+	void show() {
+		System.out.println("Class B: i + j = " + (i + j));
+	}
+	void sampe() {
+		System.out.println("from class b");
+	}
+}
+
+
+public class Test4 {
+	public static void main(String[] args) {
+		
+		 A a=new B();
+		 a.display();
+	//	 a.show(); 
+		 
+		 
+	}
+
+}
